3b92a9ce8e9726a760d6b21372f8f5bf702292cb
Sumair <90900352+sumairruhani1@users.noreply.github.com>
2022-12-29 16:53:12 +0000

    functionality for plotting vector of boats after parsing config

--- main.cpp
+++ main.cpp
@@ -1,54 +1,28 @@
 #include "./include/index.hpp"
 
+// Boat class
 class Boat {
- public:
+public:
   int length;
   std::string name;
   bool isPlaced;
 
-  Boat(int length, const std::string& name, bool isPlaced)
+  Boat(int length, const std::string &name, bool isPlaced)
       : length(length), name(name), isPlaced(isPlaced) {}
 };
 
-void importBoatConfigData(std::string filename, std::vector<Boat>& boats) {
-  // file handling for adaship_config.ini file
-  std::ifstream file;
-  file.open(filename); // open file
-  std::string line;
-  if (file) {
-    while (std::getline(file, line)) {
-      line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
-
-      if (line.find("Boat") != -1) {
-        int startPointer = line.find(":") + 1;           // Initial pointer
-        int endPointer = line.find(",") + 1; // Secondary pointer
-        int diff = endPointer - startPointer;            // string difference
-        std::string name = line.substr(startPointer, diff - 1);
-        int length = std::stoi(line.substr(endPointer));
-        boats.emplace_back(length, name, false);
-      }
-    }
-  }
-  else {
-    std::cout << "Could not find or open adaship_config.ini" << std::endl;
-  }
-};
-
-
-
+// Board class
 class Board {
- public:
+public:
   static int boardHeight; // height of the game board
-  static int boardWidth; // width of the game board
-
+  static int boardWidth;  // width of the game board
 
   // represent a cell on the game board
   class Cell {
-   public:
+  public:
     Cell() : isShip(false) {}
     bool isShip; // whether there is a ship at this cell
   };
-
 
   // initialize the game board
   Board() {
@@ -61,40 +35,41 @@ class Board {
     }
   }
 
-
   // check if a cell is within the bounds of the game board
   bool isValidCell(int x, int y) {
     return x >= 0 && x < boardWidth && y >= 0 && y < boardHeight;
   }
 
   bool canPlaceShip(int x, int y, int length, bool isVertical) {
-  // check if the ship is within the bounds of the game board
-  if (isVertical) {
-    if (!isValidCell(x, y) || !isValidCell(x, y + length - 1)) {
-      return false;
+    // check if the ship is within the bounds of the game board
+    if (isVertical) {
+      if (!isValidCell(x, y) || !isValidCell(x, y + length - 1)) {
+        return false;
+      }
+    } else {
+      if (!isValidCell(x, y) || !isValidCell(x + length - 1, y)) {
+        return false;
+      }
     }
-  } else {
-    if (!isValidCell(x, y) || !isValidCell(x + length - 1, y)) {
-      return false;
-    }
-  }
 
-  // check if there is a ship at any of the cells occupied by the ship
-  if (isVertical) {
-    for (int i = y; i < y + length; i++) {
-      if (gameBoard[i][x].isShip) {  // check if the cell is already occupied by a ship
-        return false;
+    // check if there is a ship at any of the cells occupied by the ship
+    if (isVertical) {
+      for (int i = y; i < y + length; i++) {
+        if (gameBoard[i][x]
+                .isShip) { // check if the cell is already occupied by a ship
+          return false;
+        }
+      }
+    } else {
+      for (int i = x; i < x + length; i++) {
+        if (gameBoard[y][i]
+                .isShip) { // check if the cell is already occupied by a ship
+          return false;
+        }
       }
     }
-  } else {
-    for (int i = x; i < x + length; i++) {
-      if (gameBoard[y][i].isShip) {  // check if the cell is already occupied by a ship
-        return false;
-      }
-    }
+    return true;
   }
-  return true;
-}
 
   // place a ship on the game table
   void placeShip(int x, int y, int length, bool isVertical) {
@@ -108,78 +83,51 @@ class Board {
   }
 
   // plot a ship on the game table
-  void plotShip(const std::string& position, int length, bool isVertical) {
+  void plotShip(const std::string &position, Boat &boat, bool isVertical) {
     // parse the position string
-    std::stringstream stream(position);  // create a stringstream from the position string
-    char col;
+    std::stringstream stream(
+        position); // create a stringstream from the position string
+    char column;
     int row;
-    stream >> col >> row;  // read the column and row from the stringstream
-    int x, y;
-    if (isalpha(col)) {
-      x = col - 'A';
-      y = row - 1;
-    } else {
-      x = row - 1;
-      stream >> col;  // read the column character from the stringstream
-      y = col - 'A';
+    if (!(stream >> column >> row)) {
+      std::cout << "Invalid position" << std::endl;
+      return;
     }
-  
-    // check if the ship can be placed at the given position and orientation
-    if (canPlaceShip(x, y, length, isVertical)) {
-      placeShip(x, y, length, isVertical);
-    } else {
-      std::cout << std::endl << "Error: Cannot place ship at position " << position
-                << " in the given orientation" << std::endl << std::endl;
+    row--;                // adjust for zero-based indexing
+    int x = column - 'A'; // convert column to integer
+    int y = row;
+    int length = boat.length;
+
+    // check if the ship can be placed at the specified location
+    if (!canPlaceShip(x, y, length, isVertical)) {
+      std::cout << "Cannot place ship at specified location" << std::endl;
+      return;
     }
+
+    // place the ship on the board
+    placeShip(x, y, length, isVertical);
+    boat.isPlaced = true; // mark the boat as placed
+    std::cout << boat.name << " placed at " << position << std::endl;
   }
 
-  // plot some ships on the game table
-  void plotShips() {
-    while (true) {
-      std::string position;
-      int length;
-      bool isVertical;
-      printBoard();
-    
-      // read the position, length, and orientation of the ship
-      std::cout << std::endl << "Enter position, length, and orientation for ship (e.g. A1 2 V): ";
-      std::cin >> position >> length >> std::ws;  // read the position and length, and skip any leading whitespace
-      if (position.empty()) {  // check if the position string is empty
-        return;  // exit the function
-      }
-      if (std::cin.fail()) {  // check if there was an error reading the input
-        std::cin.clear();  // clear the error flag
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // ignore the rest of the line
-        continue;  // go back to the beginning of the loop
-      }
-      char orientation;
-      std::cin >> orientation;  // read the orientation character
-      if (std::cin.fail()) {  // check if there was an error reading the input
-        std::cin.clear();  // clear the error flag
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // ignore the rest of the line
-        continue;  // go back to the beginning of the loop
-      }
-      while (orientation != 'V' && orientation != 'v' && orientation != 'H' && orientation != 'h') {  // validate the orientation input
-        std::cout << "Error: Invalid orientation. Please enter either V or H: ";
-        std::cin >> orientation;
-      }
-      isVertical = (orientation == 'V' || orientation == 'v');
-      plotShip(position, length, isVertical);
-    }
-  }
-
-    // print the game board
+  // print the game board
   void printBoard() {
     // print the alphabet along the top row
-    std::cout << std::endl << "   ";  // add an extra space to align the letters with the board cells
+    std::cout << std::endl
+              << "   "; // add an extra space to align the letters with the
+                        // board cells
     for (int i = 0; i < boardWidth; i++) {
-      std::cout << std::setw(2) << static_cast<char>('A' + i) << " ";  // use setw to align the letters
+      std::cout << std::setw(2) << static_cast<char>('A' + i)
+                << " "; // use setw to align the letters
     }
     std::cout << std::endl;
 
-    // print the numbers along the left column and the contents of the board cells
+    // print the numbers along the left column and the contents of the board
+    // cells
     for (int i = 0; i < boardHeight; i++) {
-      std::cout << std::setw(2) << i + 1 << "  ";  // use setw to align the numbers and add an extra space to align with the board cells
+      std::cout << std::setw(2) << i + 1
+                << "  "; // use setw to align the numbers and add an extra space
+                         // to align with the board cells
       for (int j = 0; j < boardWidth; j++) {
         if (gameBoard[i][j].isShip) {
           std::cout << "X  ";
@@ -191,15 +139,16 @@ class Board {
     }
   }
 
- private:
+private:
   std::vector<std::vector<Cell>> gameBoard;
 };
 
-int Board::boardHeight;  // initialize the boardHeight and boardWidth variables
-int Board::boardWidth;
+int Board::boardHeight = 10; // initialize the board height
+int Board::boardWidth = 10;  // initialize the board width
 
-
-void importBoardConfigData(std::string filename) {
+// import boat configuration data from a file
+void importBoatConfigData(const std::string &filename,
+                          std::vector<Boat> &boats) {
   // file handling for adaship_config.ini file
   std::ifstream file;
   file.open(filename); // open file
@@ -208,56 +157,87 @@ void importBoardConfigData(std::string filename) {
     while (std::getline(file, line)) {
       line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
 
-      if (line.find("Board") != -1) {
-        int startPointer = line.find(":") + 1;           // Initial pointer
-        int endPointer = line.find("x") + 1; // Secondary pointer
-        int diff = endPointer - startPointer;            // string difference
-        std::string height = line.substr(startPointer, diff - 1);
-        std::string width = line.substr(endPointer);
-        std::cout << "Board Height is " << height << " and width is " << width
-                  << std::endl;
-        Board::boardHeight = atoi(height.c_str());
-        Board::boardWidth = atoi(width.c_str());
+      if (line.find("Boat") != -1) {
+        int startPointer = line.find(":") + 1; // Initial pointer
+        int endPointer = line.find(",") + 1;   // Secondary pointer
+        int diff = endPointer - startPointer;  // string difference
+        std::string name = line.substr(startPointer, diff - 1);
+        int length = std::stoi(line.substr(endPointer));
+        boats.emplace_back(length, name, false);
       }
     }
-  }
-  else {
-    std::cout << "Could not find or open adaship_config.ini" << std::endl;
-  }
-};
-
-void tutorialInstructions() {
-  std::cout << "Welcome to AdaShip!" << std::endl <<std::endl;
-  std::cout 
-    << "AdaShip is a spinoff version of the classic battleships game and the rules are simple. \n"
-    << "Firstly, you and your opponent will tactically place ships on your boards. Following this you \n"
-    << "take turns firing torpedoes at your opponents ships with the intent of hitting all of theirs, \n"
-    << "while not getting hit yourself. \n\n"
-    << "If you destroy all your opponents ships before they can destroy yours then you have won." << std::endl << std::endl;
-}
-
-void menuOptions() {
-  int option;
-  std::cout << "Menu:" << std::endl;
-  std::cout << "1. One Player vs Computer" << std::endl;
-  std::cout << "2. Quit Game" << std::endl << std::endl;
-  std::cout << "Enter option: ";
-  std::cin >> option;
-
-  if (option == 1) {
-    importBoardConfigData("adaship_config.ini");
-    Board board;
-    board.plotShips();
-    board.printBoard();
-  } else if (option == 2) {
-    std::exit;
   } else {
-    std::cout << ("You haven't entered a valid option");
+    std::cout << "Could not find or open adaship_config.ini" << std::endl;
   }
 }
 
 int main() {
-  tutorialInstructions();
-  menuOptions();
+  std::vector<Boat> boats;
+  importBoatConfigData("adaship_config.ini", boats);
+
+  Board board;
+
+  // list the boats and allow the user to select a boat to place on the board
+  int n = 0; // counter of the number of boats that have been placed
+  while (!boats.empty()) {
+    board.printBoard();
+    int index = 0;
+    std::cout << std::endl << "Available boats:" << std::endl << std::endl;
+    for (const Boat &boat : boats) {
+      if (!boat.isPlaced) {
+        std::cout << ++index << ". " << boat.name << " [Length: " << boat.length
+                  << "]" << std::endl;
+      }
+    }
+
+    std::cout << std::endl << "Enter the number of the boat to place: ";
+    int number;
+    if (!(std::cin >> number)) {
+      std::cout << std::endl
+                << std::endl
+                << "Error: Please enter a valid boat number" << std::endl;
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+
+    // find the boat with the given number
+    auto it = std::find_if(boats.begin(), boats.end(),
+                           [&number, &n](const Boat &boat) {
+                             return --number == 0 - n && !boat.isPlaced;
+                           });
+
+    if (it == boats.end()) {
+      std::cout << "Invalid boat number or boat already placed" << std::endl;
+      continue;
+    }
+
+    Boat &boat = *it;
+    n++; // increment the counter of placed boats
+
+    std::cout << "Enter position and orientation (e.g. A1 V, B5 H etc): ";
+    std::string position;
+    char orientation;
+    if (!(std::cin >> position >> orientation)) {
+      std::cout << "Error: Ensure that you have entered acceptable coordinates "
+                   "and orientation. Note that a ship cannot be placed over "
+                   "other ships. "
+                << std::endl;
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+    bool isVertical = orientation == 'V';
+
+    // check if both position and orientation are present in the input
+    if (position.empty() || (orientation != 'V' && orientation != 'v' &&
+                             orientation != 'H' && orientation != 'h')) {
+      std::cout << std::endl << "Error: Invalid input" << std::endl;
+      continue;
+    }
+
+    board.plotShip(position, boat, isVertical);
+  }
+
   return 0;
 }
